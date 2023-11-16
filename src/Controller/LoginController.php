e4596e04ba8c5eecdbd2d919af8d31fc13eeba7f
JFlorentino45 <jarryd.florentino@gmail.com>
2023-11-16 15:48:59 +0100

    Updated login

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -9,13 +9,13 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
-    #[Route('/login', name: 'login')]
+    #[Route('/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('auth/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
     #[Route('/logout', name: 'logout')]
