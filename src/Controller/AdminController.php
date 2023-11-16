bc9eb675cf23acf6170de9555e493be38478a634
JFlorentino45 <jarryd.florentino@gmail.com>
2023-11-16 15:07:27 +0100

    created profile and admin control of profiles

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use App\Repository\UserRepository;
 use App\Repository\BlogRepository;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,10 +12,28 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminController extends AbstractController
 {
     #[Route('/blogs', name: 'app_admin_blogs', methods: ['GET'])]
-    public function index(BlogRepository $blogRepository): Response
+    public function getABlogs(BlogRepository $blogRepository): Response
     {
         return $this->render('admin/blogs.html.twig', [
             'blogs' => $blogRepository->findAllOrderedByLatest(),
+        ]);
+    }
+
+    #[Route('/users', name: 'app_admin_users', methods: ['GET'])]
+    public function getAUsers(UserRepository $userRepository): Response
+    {
+        return $this->render('admin/users.html.twig', [
+            'users' => $userRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/user-blogs/{id}', name: 'app_admin_userblogs')]
+    public function getUserBlogs($id, UserRepository $userRepository, BlogRepository $blogRepository): Response
+    {
+        $user = $userRepository->find($id);
+        return $this->render('admin/user_blogs.html.twig', [
+            'blogs' => $blogRepository->findBy(['createdBy' => $id]),
+            'userName' => $user->getUsername(),
         ]);
     }
 }
