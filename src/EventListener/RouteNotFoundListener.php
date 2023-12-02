9a11a8bfd220fa475e4c6ffd92e54b427b13e76a
JFlorentino45 <jarryd.florentino@gmail.com>
2023-12-02 16:05:04 +0100

    Added event listener for routenotfound

---
+++ src/EventListener/RouteNotFoundListener.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\RouterInterface;
+
+class RouteNotFoundListener
+{
+    private $router;
+
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    public function onKernelException(ExceptionEvent $event)
+    {
+        $exception = $event->getThrowable();
+
+        if ($exception instanceof NotFoundHttpException) {
+            $url = $this->router->generate('app_blog_index');
+            $response = new RedirectResponse($url);
+            $event->setResponse($response);
+        }
+    }
+}
