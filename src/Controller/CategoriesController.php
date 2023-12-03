a053f06c859e41722cb6f7a9a48c5e7d9c228ced
JFlorentino45 <jarryd.florentino@gmail.com>
2023-12-03 22:12:47 +0100

    Cleaned up CategoriesController

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Categories;
 use App\Repository\BlogRepository;
 use App\Repository\CategoriesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,16 +14,29 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/categories')]
 class CategoriesController extends AbstractController
 {
+
+    private $blogRepository;
+    private $categoryRepository;
+
+    public function __construct(
+        BlogRepository $blogRepository,
+        CategoriesRepository $categoryRepository
+    ) {
+        $this->blogRepository = $blogRepository;
+        $this->categoryRepository = $categoryRepository;
+    }
+
     #[Route('/blogs/{id}', name: 'app_blog_category')]
-    public function catBlogs(BlogRepository $blogRepository,int $id, CategoriesRepository $categoryRepository): Response
+    public function catBlogs(Categories $categories): Response
     {
         $url = 'catBlogs';
-        $category = $categoryRepository->find($id);
+        $id = $categories->getId();
+        $category = $this->categoryRepository->find($id);
         $name = $category->getCategory();
-        $categories = $categoryRepository->findAll();
+        $categories = $this->categoryRepository->findAll();
 
         return $this->render('home/categoryIndex.html.twig', [
-            'blogs' => $blogRepository->findCategoryOrderedByLatest($id),
+            'blogs' => $this->blogRepository->findCategoryOrderedByLatest($id),
             'url' => $url,
             'categories' => $categories,
             'category' => $name,
@@ -31,11 +45,11 @@ class CategoriesController extends AbstractController
     }
 
     #[Route('/load-blogs/{id}', name: 'app_blog_category_more')]
-    public function loadCatBlogs(BlogRepository $blogRepository, $id, Request $request): JsonResponse
+    public function loadCatBlogs(Categories $categories, Request $request): JsonResponse
     {
-
+        $id = $categories->getId();
         $offset = $request->query->get('offset', 0);
-        $blogs = $blogRepository->findMoreCategoryBlogs($offset, $id);
+        $blogs = $this->blogRepository->findMoreCategoryBlogs($offset, $id);
 
         $html = $this->renderView('home/_cat_items.html.twig', ['blogs' => $blogs]);
 
@@ -43,10 +57,11 @@ class CategoriesController extends AbstractController
     }
 
     #[Route('/search-blogs/{id}', name: 'search_cat_blogs', methods: ['GET'])]
-    public function searchBlogs(Request $request, BlogRepository $blogRepository, $id): JsonResponse
+    public function searchBlogs(Request $request, Categories $categories): JsonResponse
     {
+        $id = $categories->getId();
         $searchTerm = $request->query->get('term');
-        $blogs = $blogRepository->searchCatBlogs($searchTerm, $id);
+        $blogs = $this->blogRepository->searchCatBlogs($searchTerm, $id);
 
         $html = $this->renderView('home/_blog_items.html.twig', ['blogs' => $blogs]);
 
